0c720e5512f92d32d906900d579d8a573081d625
vmuijrers <vmuijrers@gmail.com>
2016-09-01 16:31:11 +0200

    More Clean Slate

--- Toets_2/Toets_2_Beginner/Main/Score.py
+++
@@ -1,7 +0,0 @@
-class Score(object):
-    def __init__(self,score):
-        self.score = score
-    def addScore(self):
-        self.score += 1000
-    def display(self):
-        text(self.score,width/2,40)
